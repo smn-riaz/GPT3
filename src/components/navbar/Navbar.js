957b9d1e60b3e32ac4916da105f0effab43fd2c7
shahman-riaz <shahmanriaz07@gmail.com>
2023-08-16 06:19:15 +0600

    completed

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -11,7 +11,7 @@ const Menu = () => (
       <a href="#home">Home</a>
     </p>
     <p>
-      <a href="#wgpt3">What is GPT3</a>
+      <a href="#whpt3">What is GPT3</a>
     </p>
     <p>
       <a href="#possibility">Open AI</a>
